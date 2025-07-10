d782ed89f85da99ba079e3ebdab51c2c8a7b2296
GuluuBest <ridhollah020@gmail.com>
2025-07-10 23:37:42 +0700

    Update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import ProfilePage from "./pages/ProfilePage";
 import CreateListingPage from "./pages/CreateListingPage";
 import ListingDetailPage from "./pages/ListingDetailPage";
 import EditListingPage from "./pages/EditListingPage";
+import "./index.css";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,22 +1,8 @@
-// client/tailwind.config.js
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {
-      // GABUNGKAN SEMUA DI SINI
-      colors: {
-        primary: "#1A659E",
-        secondary: "#2482C5",
-        background: "#F3F4F6",
-        surface: "#FFFFFF",
-        "text-primary": "#212529",
-        "text-secondary": "#6C757D",
-      },
-      fontFamily: {
-        sans: ["Poppins", "sans-serif"],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
